61afa23ab2bc05f6b02b0a0fae79c14fb28072ab
Daniel Nunes <danielnunesgnu@gmail.com>
2020-09-09 15:04:33 -0300

    feat: Connecting with the back-end

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://localhost:8081.com/',
+  // baseURL: 'https://localhost:8081.com/', localhost
+  baseURL: 'https://poke-list-app-back.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
